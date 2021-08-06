1bc164e27212eae309fe3b275b78b532a1e9df96
Mohamed Doffer Mohamed Nasir <73598690+mohamed-nasir@users.noreply.github.com>
2021-08-06 14:06:40 +0800

    v0.0.3

---
+++ src/widgets/Menu/components/CakePrice.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import { PancakeRoundIcon } from "../../../components/Svg";
+import Text from "../../../components/Text/Text";
+import Skeleton from "../../../components/Skeleton/Skeleton";
+
+interface Props {
+  cakePriceUsd?: number;
+}
+
+const PriceLink = styled.a`
+  display: flex;
+  align-items: center;
+  svg {
+    transition: transform 0.3s;
+  }
+  :hover {
+    svg {
+      transform: scale(1.2);
+    }
+  }
+`;
+
+const CakePrice: React.FC<Props> = ({ cakePriceUsd }) => {
+  return cakePriceUsd ? (
+    <PriceLink href="https://dex.guru/token/0x9A2af686fFaFf9B10C8B800c2261155ca9D3c5c2-bsc" target="_blank">
+      <PancakeRoundIcon width="24px" mr="8px" />
+      <Text color="textSubtle" bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
+    </PriceLink>
+  ) : (
+    <Skeleton width={80} height={24} />
+  );
+};
+
+export default React.memo(CakePrice);
